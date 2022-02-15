00708b74c355d7686138663e223d762b5de0ecce
Aframmm <89247875+Aframmm@users.noreply.github.com>
2022-02-15 15:34:16 +0100

    Add files via upload

---
+++ Sortering 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Sortering2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>();
+
+            Random ran = new Random();
+
+            Stopwatch watch = new Stopwatch();
+
+            for(int i = 0; intList.Count < 1000; i++){
+                intList.Add(ran.Next(1, 1001));
+            }
+            watch.Start();
+
+            int first;
+            for (int i = 0; i < intList.Count - 1; i++){
+                first = i;
+                for(int j = first; j < intList.Count; j++){
+                    if (intList[j] < intList[first]){
+                        int abc = intList[j];
+                        intList[j] = intList[first];
+                        intList[first] = abc;
+                    }
+                }
+            }
+            watch.Stop();
+    
+            foreach(int i in intList){
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine(watch.ElapsedMilliseconds);
+        }
+    }
+}
